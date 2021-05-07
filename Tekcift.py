6ae95db5e83dd16db9117e65a308e2fe109363a4
seymatw <79170484+seymatw@users.noreply.github.com>
2021-05-07 23:13:51 +0300

    Add files via upload

---
+++ Tekcift.py
@@ -0,0 +1,10 @@
+while True : 
+    number= int (input("enter any number expect 0 ") )
+    if number!=0 :
+        if (number%2==0) :
+            print (number, " is an even number ")
+            
+        else :
+            print (number " is an odd number ")
+        
+    
---
+++ temizKelime.py
@@ -0,0 +1,14 @@
+s1="Ah5me4t"
+s2="M9eHm4eT"
+s3 ="Ha3K5a1n"
+
+s1=s1.replace("5","")
+s1=s1.replace("4","")
+
+s2=s2.replace("9","")
+s2=s2.replace("4","")
+s3=s3.replace("3","")
+s3=s3.replace("5","")
+s3=s3.replace("1","")
+
+print(s1,"-",s2,"-",s3)
